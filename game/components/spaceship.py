135aee11b198d50f8bb2aa039405e5bd22e5d308
DrEncoder <miguel.cortes@fundacion-jala.org>
2023-06-16 03:08:32 -0600

    Tarea 1

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -18,10 +18,33 @@ class Spaceship:
         if user_input[pygame.K_LEFT]:
            self.move_left()
 
+        elif user_input[pygame.K_RIGHT]:
+            self.move_right()
+
+        elif user_input[pygame.K_UP]:
+            self.move_up()
+
+        elif user_input[pygame.K_DOWN]:
+            self.move_down()
+
+
      def draw(self, screen):
         screen.blit(self.image, self.rect)
 
      def move_left(self):
         if self.rect.left > 0:
               self.rect.x -= 10
+     
+     def move_right(self):
+        if self.rect.right > 0:
+            self.rect.x += 10  
+
+     def move_up(self):
+         if self.rect.top > 0:
+            self.rect.y -= 10      
+
+     def move_down(self):
+         if self.rect.bottom > 0:
+            self.rect.y += 10    
+
      
